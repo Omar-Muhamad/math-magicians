992043c2dd49fa8e78ad020f5c2d0d37fb5601f6
omar-muhamad <eng.omarmuhammad@gmail.com>
2022-01-07 03:18:00 +0200

    Change App to class-based component & Remove unused files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Calculator />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Calculator />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
