8227d825ccb6a6e80ca323752572e85aba973e4a
omar-muhamad <eng.omarmuhammad@gmail.com>
2022-01-08 08:16:06 +0200

    Refactor all class-based component to functional component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Calculator />
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Calculator />
+    </header>
+  </div>
+);
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,51 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Calculator = () => {
+  const [state, setState] = useState({});
 
-  clickHandler = (e) => {
-    const result = calculate(this.state, e.target.textContent);
-    this.setState(result);
-  }
+  const clickHandler = (e) => {
+    const result = calculate(state, e.target.textContent);
+    setState(result);
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
+  const { total, next, operation } = state;
 
-    return (
-      <div className="container">
-        <div className="output" id="output">
-          {total && total}
-          {operation && operation}
-          {next && next}
-        </div>
-        <div className="calculatorBtns">
-          <button className="acBtn gray" type="button" onClick={this.clickHandler}>AC</button>
-          <button className="plusMinusBtn gray" type="button" onClick={this.clickHandler}>+/-</button>
-          <button className="PercentageBtn gray" type="button" onClick={this.clickHandler}>%</button>
-          <button className="operatorBtn" type="button" onClick={this.clickHandler}>÷</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>7</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>8</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>9</button>
-          <button className="operatorBtn" type="button" onClick={this.clickHandler}>x</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>4</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>5</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>6</button>
-          <button className="operatorBtn" type="button" onClick={this.clickHandler}>-</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>1</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>2</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>3</button>
-          <button className="operatorBtn" type="button" onClick={this.clickHandler}>+</button>
-          <button className="digitBtn span2" type="button" onClick={this.clickHandler}>0</button>
-          <button className="digitBtn" type="button" onClick={this.clickHandler}>.</button>
-          <button className="evaluationBtn" type="button" onClick={this.clickHandler}>=</button>
-        </div>
+  return (
+    <div className="container">
+      <div className="output" id="output">
+        {total && total}
+        {operation && operation}
+        {next && next}
       </div>
-    );
-  }
-}
+      <div className="calculatorBtns">
+        <button className="acBtn gray" type="button" onClick={clickHandler}>AC</button>
+        <button className="plusMinusBtn gray" type="button" onClick={clickHandler}>+/-</button>
+        <button className="PercentageBtn gray" type="button" onClick={clickHandler}>%</button>
+        <button className="operatorBtn" type="button" onClick={clickHandler}>÷</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>7</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>8</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>9</button>
+        <button className="operatorBtn" type="button" onClick={clickHandler}>x</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>4</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>5</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>6</button>
+        <button className="operatorBtn" type="button" onClick={clickHandler}>-</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>1</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>2</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>3</button>
+        <button className="operatorBtn" type="button" onClick={clickHandler}>+</button>
+        <button className="digitBtn span2" type="button" onClick={clickHandler}>0</button>
+        <button className="digitBtn" type="button" onClick={clickHandler}>.</button>
+        <button className="evaluationBtn" type="button" onClick={clickHandler}>=</button>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
